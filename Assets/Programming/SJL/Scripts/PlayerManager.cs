c495aa80487d7059925c72dd7ada218d9a86e80b
sejun-2 <166081523+sejun-2@users.noreply.github.com>
2025-08-18 17:47:32 +0900

    [feat] Implementing a player list
    플레이어 정보를 임시로 만들어 플레이어 리스트를 프리펩으로 구현

---
+++ Assets/Programming/SJL/Scripts/PlayerManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SJL
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        [SerializeField] Button playerRecruitmentButton;
+
+        public List<Player> playerList = new List<Player>();
+
+        [SerializeField] public GameObject playerUIPrefab;
+        [SerializeField] public Transform playerListPanel; // 선수들을 담을 부모 오브젝트
+
+
+        private void Start()
+        {
+            // 샘플 데이터
+            playerList.Add(new Player { name = "신재원", grade = "일반등급", age = 21, type = "C" });
+            playerList.Add(new Player { name = "이민호", grade = "국대 후보", age = 20, type = "B" });
+            playerList.Add(new Player { name = "민만준", grade = "국가대표", age = 22, type = "A" });
+            playerList.Add(new Player { name = "안정환", grade = "일반등급", age = 19, type = "C" });
+            playerList.Add(new Player { name = "이규진", grade = "국대 후보", age = 24, type = "B" });
+            playerList.Add(new Player { name = "신희관", grade = "국가대표", age = 26, type = "A" });
+
+            playerRecruitmentButton.onClick.AddListener(DisplayPlayers);
+        }
+
+        private void DisplayPlayers()
+        {
+            for (int i = 0; i < playerList.Count; i++)
+            {
+                GameObject go = Instantiate(playerUIPrefab, playerListPanel);
+                PlayerUI ui = go.GetComponent<PlayerUI>();
+                ui.SetPlayer(playerList[i]);
+            }
+        }
+    }
+
+
+    [Serializable]
+    public class Player
+    {
+        public String name;
+        public int age;
+        public string grade;
+        public string type;
+
+        public int stamina;
+        public int agility;
+        public int flexibility;
+        public int technique;
+        public int speed;
+        public int balance;
+        public int fatigue;
+        public int mental;
+    }
+}
---
+++ Assets/Programming/SJL/Scripts/PlayerUI.cs
@@ -0,0 +1,25 @@
+﻿using SJL;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SJL
+{ 
+    public class PlayerUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI nameText;
+        [SerializeField] private TextMeshProUGUI gradeText;
+        [SerializeField] private TextMeshProUGUI ageText;
+        [SerializeField] private TextMeshProUGUI typeText;
+
+        public void SetPlayer(Player player)
+        {
+            nameText.text = player.name;
+            gradeText.text = player.grade;
+            ageText.text = player.age.ToString();
+            typeText.text = player.type;
+        }
+    }
+}
