a22ddb54dc2106467652fa32bb6c64808ec0cbad
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-08-19 01:51:38 +0900

    [Init] Initialize Athlete System Structures
    - 선수 구조 설계
    - 선수 시스템에 맞게 스크립트들 파일만 생성함

---
+++ Assets/Programming/JYL/Scripts/DomAthModel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 캐릭터의 정보를 CSV에서 읽어와서 만드는 캐릭터 정보 객체
+public class CharacterModel : MonoBehaviour
+{
+    public void Init()
+    {
+        
+    }
+}
