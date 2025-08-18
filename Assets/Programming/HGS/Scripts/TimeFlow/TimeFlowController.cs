dd07d5880579e5baaa3404455458d9090ad6c420
bart <bartshin@icloud.com>
2025-08-19 01:03:47 +0900

    [Feat] 시간 흐름 UI

--- Assets/Programming/HGS/Scripts/TimeFlow/TimeFlowController.cs
+++ Assets/Programming/HGS/Scripts/TimeFlow/TimeFlowController.cs
@@ -1,5 +1,6 @@
 using System;
 using UniRx;
+using UnityEngine;
 
 namespace SHG
 {
---
+++ Assets/StatefulUISupport/Scripts/Roles/ScrollerRole.cs
@@ -0,0 +1,8 @@
+// ReSharper disable CheckNamespace
+// ReSharper disable UnusedMember.Global
+[StatefulUI.Runtime.RoleAttributes.ScrollerRoleAttribute]
+public enum ScrollerRole
+{
+    Unknown = 0,
+
+}
