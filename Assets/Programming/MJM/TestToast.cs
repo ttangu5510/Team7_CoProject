2806e9dfab28edbaf21c17220a1c63c60ca1cb8a
mjmin99 <mjmin99@naver.com>
2025-08-20 10:39:43 +0900

    [Feat] UIManager 초안 작성
    - UI 매니저의 초안을 작성함
    - 수정 전 임시 커밋

---
+++ Assets/Programming/MJM/TestToast.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestToast : MonoBehaviour
+{
+    [SerializeField] private GameObject popupToastPrefab;
+    [SerializeField] private Transform popupsParentTransform;
+
+    public void ShowToast()
+    {
+        var toast = Instantiate(popupToastPrefab, popupsParentTransform);
+        toast.GetComponent<Toast>().SetText("This is Just Toast!");
+        UIManager.Instance.ShowPopup(toast);
+    }
+}
---
+++ Assets/Programming/MJM/Toast.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class Toast : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private float lifeTime = 1.8f;
+
+    // 활성화시 즉시 코루틴 실행
+    private void OnEnable()
+    {
+        StartCoroutine(AutoClose());
+    }
+
+    // 텍스트 내용 변경
+    public void SetText(string msg)
+    {
+        if (text) text.text = msg;
+    }
+
+    // 자동 사라짐 코루틴
+    private IEnumerator AutoClose()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        if (UIManager.Instance) UIManager.Instance.CloseSpecificPopup(gameObject);
+        else gameObject.SetActive(false);
+    }
+}
