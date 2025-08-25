4c17597132eaee52aa51c532d4d2dcd4ad994efa
sejun-2 <166081523+sejun-2@users.noreply.github.com>
2025-08-25 11:39:23 +0900

    [feat] R&D

---
+++ Assets/Programming/SJL/Scripts/Tester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SJL
+{
+    public class Tester : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+
+        public void Jump()
+        {
+            Debug.Log("점프!");
+        }
+    }
+}
