ca91bbc0f109f6bc3d45fcc0757e0c712d8f569f
sejun-2 <166081523+sejun-2@users.noreply.github.com>
2025-08-25 14:40:08 +0900

    [Build] Code modification using UniRX method
    Code modification using UniRX method

--- Assets/Programming/SJL/Scripts/Tester.cs
+++ Assets/Programming/SJL/Scripts/Tester.cs
@@ -1,22 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SJL
 {
     public class Tester : MonoBehaviour
     {
-        private void Update()
+        [SerializeField] Rigidbody2D rigid;
+        [SerializeField] bool isGeound;
+
+        [SerializeField] Button button;
+
+        private void Awake()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Jump();
-            }
+            button.OnPointerEnterAsObservable() // OnPointerEnterAsObservable() : 포인터가 버튼 위에 올라갔을 때 이벤트를 Observable로 변환
+                .Subscribe(_ => Debug.Log("버튼 위에 포인터 올라감")); // .Sebscribe() : 동작에 대한 정의
+
+            button.OnClickAsObservable() // OnClickAsObservable() : 버튼 클릭 이벤트를 Observable로 변환
+                .Subscribe(_ => Debug.Log("버튼 클릭됨")); // .Sebscribe() : 동작에 대한 정의
+
+            gameObject.UpdateAsObservable() // UpdateAsObservable() : Update() 함수를 Observable로 변환
+                .Where(x => Input.GetKeyDown(KeyCode.Space))    // .Where() : 조건에 대한 정의
+                .Where(x => isGeound == true)    // .Where() : 조건에 대한 정의
+                .Subscribe(x => rigid.AddForce(Vector3.up * 10, ForceMode2D.Impulse));    // .Sebscribe() : 동작에 대한 정의
+
+            //이동 -> 좋은 방법은 아님
+            gameObject.UpdateAsObservable()
+                .Where(x => Input.GetAxis("Horizontal") != 0)
+                .Subscribe(x => transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime));
+
+            //gameObject.UpdateAsObservable()
+            //    .Where(x => Input.GetAxis("Vertical") != 0)
+            //    .Subscribe(x => transform.Translate(Vector3.up * Input.GetAxis("Vertical")));
+
+            //땅 착지
+            gameObject.OnCollisionEnter2DAsObservable() // OnCollisionEnter2DAsObservable() : OnCollisionEnter2D() 함수를 Observable로 변환
+                .Where(collision => collision.collider.tag == "Ground")
+                .Subscribe(x => isGeound = true); // .Sebscribe() : 동작에 대한 정의
+
+            //gameObject.OnCollisionEnter2DAsObservable() // OnCollisionEnter2DAsObservable() : OnCollisionEnter2D() 함수를 Observable로 변환
+            //    .Where(x => x.gameObject.CompareTag("Ground")) // .Where() : 조건에 대한 정의
+            //    .Subscribe(x => isGeound = true); // .Sebscribe() : 동작에 대한 정의
+
+            //땅 떨어짐
+            gameObject.OnCollisionExit2DAsObservable() // OnCollisionExit2DAsObservable() : OnCollisionExit2D() 함수를 Observable로 변환
+                .Where(collision => collision.collider.tag == "Ground")
+                .Subscribe(x => isGeound = false); // .Sebscribe() : 동작에 대한 정의
+
+            
+
         }
 
-        public void Jump()
-        {
-            Debug.Log("점프!");
-        }
+        
     }
 }
--- Assets/Programming/SJL/Scripts/TrainingCenter.cs
+++ Assets/Programming/SJL/Scripts/TrainingCenter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -21,44 +22,42 @@ namespace SJL
 
         private void Awake()
         {
-            // 버튼 클릭 이벤트 등록
-            FacilityInformationButton.onClick.AddListener(OnFacilityInformationClicked);
-            trainingButton.onClick.AddListener(OnTrainingClicked);
-            specialTrainingButton.onClick.AddListener(OnSpecialTrainingClicked);
+            // UniRx로 버튼 클릭 처리
+            FacilityInformationButton.OnClickAsObservable()
+                .Subscribe(_ => ShowPanel(PanelType.FacilityInformation)).AddTo(this);
+
+            trainingButton.OnClickAsObservable()
+                .Subscribe(_ => ShowPanel(PanelType.Training)).AddTo(this);
+
+            specialTrainingButton.OnClickAsObservable()
+                .Subscribe(_ => ShowPanel(PanelType.SpecialTraining)).AddTo(this);
         }
 
-        private void OnFacilityInformationClicked()
-        {
-            // 설명 텍스트 설정
-            explanatoryText.text = "시설 : 0 단계\n" +
-                                   "훈련 스탯 추가 수치 : + 0\n" +
-                                   "훈련과 특훈 진행 가능.";
-            // 시설 정보 박스 활성화
-            FacilityInformationBox.SetActive(true);
-            TrainingBox.SetActive(false);
-            specialTrainingBox.SetActive(false);
-        }
+        private enum PanelType { FacilityInformation, Training, SpecialTraining }
 
-        private void OnTrainingClicked()
+        private void ShowPanel(PanelType type)
         {
-            // 설명 텍스트 설정
-            explanatoryText.text = "선수틀을 배치하여 훈련시킬 수 있습니다.\n" +
-                                   "루틴에 따라 상승하는 능력치가 달라집니다..";
-            // 훈련 박스 활성화
-            FacilityInformationBox.SetActive(false);
-            TrainingBox.SetActive(true);
-            specialTrainingBox.SetActive(false);
-        }
-
-        private void OnSpecialTrainingClicked()
-        {
-            // 설명 텍스트 설정
-            explanatoryText.text = "선수틀을 배치하여 특별 훈련시킬 수 있습니다.\n" +
-                                   "루틴에 따라 상승하는 능력치가 달라집니다..";
-            // 특별 훈련 박스 활성화
-            FacilityInformationBox.SetActive(false);
-            TrainingBox.SetActive(false);
-            specialTrainingBox.SetActive(true);
+            switch (type)
+            {
+                case PanelType.FacilityInformation:
+                    explanatoryText.text = "시설 : 0 단계\n훈련 스탯 추가 수치 : + 0\n훈련과 특훈 진행 가능.";
+                    FacilityInformationBox.SetActive(true);
+                    TrainingBox.SetActive(false);
+                    specialTrainingBox.SetActive(false);
+                    break;
+                case PanelType.Training:
+                    explanatoryText.text = "선수틀을 배치하여 훈련시킬 수 있습니다.\n루틴에 따라 상승하는 능력치가 달라집니다.";
+                    FacilityInformationBox.SetActive(false);
+                    TrainingBox.SetActive(true);
+                    specialTrainingBox.SetActive(false);
+                    break;
+                case PanelType.SpecialTraining:
+                    explanatoryText.text = "선수틀을 배치하여 훈련시킬 수 있습니다.\n루틴에 따라 상승하는 능력치가 달라집니다.";
+                    FacilityInformationBox.SetActive(false);
+                    TrainingBox.SetActive(false);
+                    specialTrainingBox.SetActive(true);
+                    break;
+            }
         }
 
 
