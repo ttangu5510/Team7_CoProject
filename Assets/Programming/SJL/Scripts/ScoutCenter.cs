4838d3ac6e69678b861046cadf7aec5866da518b
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-08-28 02:05:46 +0900

    [Feat] Coneect Scout center, training center ui
    - 만준님의 예시 UI에 onClick 이벤트로 게임오브젝트 SetActive true하는 식으로 UI 연결함. 스카우트 센터, 훈련 센터 연결완료

--- Assets/Programming/SJL/Scripts/ScoutCenter.cs
+++ Assets/Programming/SJL/Scripts/ScoutCenter.cs
@@ -16,12 +16,14 @@ public class ScoutCenter : MonoBehaviour
     [Header("Text")]
     [SerializeField] private TextMeshProUGUI explanatoryText;
 
+    [Header("GameObject")] 
+    [SerializeField] private GameObject scoutCenterCanvas;
     [SerializeField] private StatefulComponent statefulComponent;
 
     private void Awake()
     {
         closeButton.OnClickAsObservable()
-            .Subscribe(_ => this.gameObject.SetActive(false)).AddTo(this);
+            .Subscribe(_ => scoutCenterCanvas.SetActive(false)).AddTo(this);
         FacilityInformation.OnClickAsObservable()
             .Subscribe(_ => { statefulComponent.SetState((int)StateRole.Active);
                 statefulComponent.SetRawTextByRole((int)TextRole.ExplanatioryText, "스카우트 센터에서는 선수와 코치를 영입할 수 있습니다.\n영입 시 구단의 예산이 소모됩니다.\n\n영입한 선수와 코치는 팀 관리에서 확인할 수 있습니다.");
