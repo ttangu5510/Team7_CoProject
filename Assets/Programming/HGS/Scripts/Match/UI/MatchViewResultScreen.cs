bf3d063d6455c5bf501d8e5216fa203e91bf35b9
bart <bartshin@icloud.com>
2025-08-29 11:09:29 +0900

    [Feat] Match result ui

--- Assets/Programming/HGS/Scripts/Match/UI/MatchViewResultScreen.cs
+++ Assets/Programming/HGS/Scripts/Match/UI/MatchViewResultScreen.cs
@@ -44,28 +44,58 @@ namespace SHG
         this.view.SetState((int)StateRole.International);
       }
       List<MatchResult> results = match.GetResults();
+      results.Sort(this.CompareMatchResult);
       this.container.Clear();
-      this.container.FillWithItems(results, this.UpdateRow);
-      foreach (var result in results) {
-        Debug.Log($"country: {result.Country.Name}, type: {result.Type}"); 
-        if (result.Type == MatchResult.ResultType.Domestic) {
-          Debug.Log($"rank: {result.GetDomesticRank()}");
-        }
-        var medals = result.GetMedalCounts();
-        Debug.Log($"({medals[0]}, {medals[1]}, {medals[2]})");
-      }
+      this.container.FillWithItems(
+        results,
+        (view, result) => this.UpdateRow(
+          view, result, results.IndexOf(result) + 1));
     }
 
-    void UpdateRow(StatefulComponent view, MatchResult result)
+    void UpdateRow(StatefulComponent view, MatchResult result, int rank)
     {
       bool isDomestic = result.Type == MatchResult.ResultType.Domestic;
 
       if (isDomestic) {
         view.SetState((int)StateRole.Domestic);
+        view.SetRawTextByRole(
+          (int)TextRole.AthleteNameLabel,
+          $"{result.GetDomesticAthlete().Name}");
+        string rankText = rank switch {
+          1 => "Gold",
+          2 => "Silver",
+          3 => "Bronze",
+          _ => string.Empty
+        };
+        view.SetRawTextByRole(
+          (int)TextRole.MedalLabel, rankText);;
       }
       else {
         view.SetState((int)StateRole.International);
+        var medals = result.GetMedalCounts();
+        view.SetRawTextByRole(
+          (int)TextRole.MedalLabel,
+          $"G: {medals[0]} S: {medals[1]} B: {medals[2]}");
+        view.SetRawTextByRole(
+          (int)TextRole.NationalityLabel, result.Country.Name);
       }
+      view.SetRawTextByRole(
+        (int)TextRole.RankLabel, $"{rank}위");
+      int point = result.CalcPoint();
+      view.SetRawTextByRole(
+        (int)TextRole.TotalLabel, $"{point}");
+    }
+
+    int CompareMatchResult(MatchResult lhs, MatchResult rhs)
+    {
+      var lhsPoint = lhs.CalcPoint();
+      var rhsPoint = rhs.CalcPoint();
+      if (lhsPoint != rhsPoint) {
+        return (lhsPoint > rhsPoint ? -1: 1);
+      }
+      var lhsRank = lhs.GetHighestRank();
+      var rhsRank = rhs.GetHighestRank();
+      return (lhsRank > rhsRank ? - 1: 1);
     }
   }
 }
