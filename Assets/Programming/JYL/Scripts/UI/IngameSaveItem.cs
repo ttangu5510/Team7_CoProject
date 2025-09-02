938ce85e1391bece8962f0a1994f2b362194bbe7
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-09-03 01:54:22 +0900

    [Feat] IngameScene Save ui mock up
    - 인게임 씬에서 사용될 세이브 UI 목업 중
    - 스크립팅 동시 진행 중

---
+++ Assets/Programming/JYL/Scripts/UI/IngameSaveItem.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameSaveItem : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is calle once per frame
+    void Update()
+    {
+        
+    }
+}
