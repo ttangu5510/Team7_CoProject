efd0f0ecfbe1e14cf8da16243bd9a4a95de663ad
sejun-2 <166081523+sejun-2@users.noreply.github.com>
2025-09-02 14:00:10 +0900

    [feat] 스카우트 센터 선수목록 갱신 수정

---
+++ Assets/Programming/SJL/Scripts/PlayerListUpdateBox.cs
@@ -0,0 +1,45 @@
+﻿using JYL;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject.SpaceFighter;
+
+namespace SJL
+{
+    public class PlayerListUpdateBox : MonoBehaviour
+    {
+        [SerializeField] Button playerRecruitmentButton;
+
+        [SerializeField] public GameObject playerUIPrefab;
+        [SerializeField] public Transform playerListPanel; // 선수들을 담을 부모 오브젝트
+
+        public List<DomAthEntity> playerDataList = new List<DomAthEntity>();    // 모든 선수 데이터 리스트
+
+        private void Start()
+        {
+            playerRecruitmentButton.onClick.AddListener(DisplayPlayers);
+        }
+
+        private void DisplayPlayers()
+        {
+            // 기존 UI 오브젝트 모두 제거
+            foreach (Transform child in playerListPanel)
+            {
+                Destroy(child.gameObject);
+            }
+
+            // 선수 리스트를 랜덤 셔플, 5명만 선택
+            var randomList = playerDataList.OrderBy(x => Random.value).Take(5).ToList();
+
+            foreach (var player in randomList)
+            {
+                GameObject go = Instantiate(playerUIPrefab, playerListPanel);
+                PlayerUI ui = go.GetComponent<PlayerUI>();
+                ui.SetPlayer(player);
+            }
+        }
+
+    }
+}
