bc6f99ebec699b9a4c64b41b2e313518342c1afc
mjmin99 <mjmin99@naver.com>
2025-09-09 12:54:10 +0900

    [Test] 희관님이랑 함께한 시간
    - 데이터를 받아오는 방법을 배움

---
+++ Assets/Programming/MJM/Test/InfoUIController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JYL;
+using UnityEngine;
+using Zenject;
+
+
+
+public class InfoUIController : MonoBehaviour
+{
+    [Inject]
+
+    DomAthService _DomAthService;
+
+    private void Awake()
+    { 
+        Debug.Log(PlayerNumber());
+
+
+    }
+
+    public string PlayerNumber()
+    {
+        _DomAthService.GetAllRecruitedAthleteList();
+
+
+
+        return "야호~";
+    }
+
+    public string RetiredPlayerNumber()
+    {
+
+        return "";
+    }
+
+    public string RetiredCoachNumber()
+    {
+
+        return "";
+    }
+
+    public string MatchNumber()
+    {
+
+        return "";
+    }
+
+    public string EarnMedalNumber()
+    {
+
+        return "";
+    }
+
+    public string FameNumber()
+    {
+
+        return "";
+    }
+
+    public string AchievementNumber()
+    {
+
+        return "";
+    }
+
+}
+
