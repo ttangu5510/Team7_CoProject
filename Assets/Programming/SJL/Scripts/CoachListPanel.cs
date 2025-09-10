fdebe0d0f0317ff10545d724e1037b5f2e91f8c2
sejun-2 <166081523+sejun-2@users.noreply.github.com>
2025-09-10 18:02:07 +0900

    코치 패널 스크립트 수정

---
+++ Assets/Programming/SJL/Scripts/CoachListPanel.cs
@@ -0,0 +1,49 @@
+﻿using JYL;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace SJL
+{
+    public class CoachListPanel : MonoBehaviour
+    {
+        [Header("Buttons")]
+        [SerializeField] Button closeButton;
+        [SerializeField] private Button applyButton;
+        [SerializeField] private Button resetButton;
+        [Header("Set Content Transform")]
+        [SerializeField] private RectTransform popUpParent; // 선수 정보 팝업창의 부모
+        [SerializeField] Transform parentContent;   // 아이템들이 생성될 부모
+        [Header("Set Prefabs")]
+        [SerializeField] private AthleteTrainingItemUI athleteItem;
+
+        [Inject] private CoachService coachService;
+
+        private List<CoachEntity> list = new(); // 배치 가능한 전체 코치
+        
+        public void Awake()
+        {
+            applyButton.onClick.AddListener(OnClickApplyButton);
+            resetButton.onClick.AddListener(OnClickResetButton);
+            closeButton.onClick.AddListener(OnClickCloseButton);
+        }
+
+        public void OnClickApplyButton()
+        {
+            // TODO : 코치 배치 적용
+        }
+
+        public void OnClickResetButton()
+        {
+            // TODO : 코치 배치 리셋
+        }
+
+        public void OnClickCloseButton()
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+}
