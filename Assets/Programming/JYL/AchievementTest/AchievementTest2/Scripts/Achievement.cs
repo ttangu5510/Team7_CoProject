784c4291173294e0941043e186ee7a8524de3872
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-09-16 23:48:25 +0900

    [Test] Achievement System R&D 2
    - 업적 시스템 설계 R&D 2. 모바일게임같은 느낌의 업적 팝업 연출과 업적에 사용될 아이템들 추가

---
+++ Assets/Programming/JYL/AchievementTest/AchievementTest2/Scripts/Achievement.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JYL.AchievementTest02
+{
+    [Serializable]
+    public class Achievement
+    {
+        public string id; // Dynamic enum generates
+        public string title;
+        public string description;
+    }
+}
+
