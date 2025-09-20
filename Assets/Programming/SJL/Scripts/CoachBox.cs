ca4468a1879d592d5e291b3111c1238e4e052032
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-09-20 21:19:35 +0900

    [Fix] training center coach assign ui update problem after firing coach
    - 코치 방출 시, 훈련 센터에서 최신화 되지 않던 문제 해결

--- Assets/Programming/SJL/Scripts/CoachBox.cs
+++ Assets/Programming/SJL/Scripts/CoachBox.cs
@@ -50,13 +50,13 @@ namespace SJL
         {
             // 현재 코치 배치 상황 업데이트
             assignedCoaches = saveManager.GetAssignedCoaches();
+            assignedCoachDict.Clear();
 
             for (int i = 0; i < assignedCoaches.Length; i++)
             {
                 if (assignedCoaches[i] > 0)
                 {
                     assignedCoachDict[i] = coachService.FindCoachById(assignedCoaches[i]); // id로 객체 찾은다음 딕셔너리에 저장.
-                    Debug.Log($"현재 코치 아이디{assignedCoaches[i]}");
                 }
             }
             UpdateUI();
