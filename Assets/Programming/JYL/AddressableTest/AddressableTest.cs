1079341786d164d65eeece276470bb9ba5d5dc90
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-09-30 15:53:29 +0900

    [Feat] apply addressables
    - 에셋 번들에 이번 프로젝트에 사용되는 에셋들을 지정함.

--- Assets/Programming/JYL/AddressableTest/AddressableTest.cs
+++ Assets/Programming/JYL/AddressableTest/AddressableTest.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -11,20 +13,26 @@ using UnityEngine.UI;
 public class AddressableTest : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI progressText;
-    [SerializeField] private Image testImage;
-    private Sprite testSprite;
+    // [SerializeField] private Image testImage;
+    // private Sprite testSprite;
 
     private AsyncOperationHandle downHandle;
-    private void Awake()
+
+    private async void Start()
     {
-        downHandle = Addressables.DownloadDependenciesAsync("Image");
-        progressText.text = downHandle.PercentComplete.ToString("P");
-        downHandle.Completed += (AsyncOperationHandle handle) =>
+        Addressables.InitializeAsync().WaitForCompletion();
+        Addressables.CheckForCatalogUpdates().WaitForCompletion(); // 카탈로그 비교해서 수정사항들 비교해서 다운로드함.
+        downHandle = Addressables.DownloadDependenciesAsync("ALL");
+        while (!downHandle.IsDone)
         {
-            Addressables.Release(handle);
-        };
+            progressText.text = $"{downHandle.GetDownloadStatus().Percent:P}";
+            await UniTask.Yield();
+        }
         
-        testSprite = Addressables.LoadAssetAsync<Sprite>("1107").WaitForCompletion();
-        testImage.sprite = testSprite;
+        gameObject.SetActive(false);
+        
+        // testSprite = Addressables.LoadAssetAsync<Sprite>("ImageAssets/character profile/ID순/1101.png").WaitForCompletion();
+        // testImage.sprite = testSprite;
     }
+
 }
